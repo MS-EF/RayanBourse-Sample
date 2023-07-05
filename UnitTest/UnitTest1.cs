bd8957fe80f4dabbf9e2752fb8e0070a0ad7203e
Soheil <erfani.ms@gmail.com>
2023-07-05 12:17:32 +0330

    add base entity,generic repositiry

--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -1,3 +1,4 @@
+using Domain.Repositories;
 using MediatR;
 using NSubstitute;
 using System;
@@ -8,10 +9,12 @@ namespace UnitTest
     public class UnitTest1
     {
         public IMediator Mediator { get; private set; }
+        public IProductRepository Repository { get; private set; }
 
         public UnitTest1()
         {
             Mediator = Substitute.For<IMediator>();
+            Repository = Substitute.For<IProductRepository>();
         }
 
         [Fact]
